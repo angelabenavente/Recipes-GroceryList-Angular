ba67ba7ce4225f6be2c62063ea12ea05a2d4d812
angelabenavente <angela.b.almazan@gmail.com>
2020-04-01 15:18:13 +0200

    Add model for single recipes

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,7 +7,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-
+  recipes: Recipe[] =  [
+    new Recipe('A Test Recipe', 'A Test Recipe Simple', 'https://pxhere.com/en/photo/1575227?__cf_chl_jschl_tk__=646e867de3043feea120358d2e417b8a68ee47f5-1585741746-0-AXz2xFbrhGw7YAr-EIQTksDF2PSxFCBoeYgIZVqZ0arTRQEsmiqNMJ42E4GHOFrCs8V_yomxJa5o1eKqVdixYHzCouriPM2fgnZRD7NHdWT_5edPGXqBjBVGVi2SBX1Z12tYZvhrQSaORcRPl0FPATaHDMHvx1lAbgLNahFeaBN65-5784mD0RuT1kvM1btuutqiGpUentlYZd-Hu2mtiNE0c_ncRVHEJ7S6D8pEBHOrRgcm5dspJG_ztzK3GuOD_FbgaG7nmskSzLtGDrhrYPBIMfWqgi-OOMzH6D2QQvu5')
+  ];
   constructor() { }
 
   ngOnInit(): void {
