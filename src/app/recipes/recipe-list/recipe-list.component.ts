a6bb89f382daab209422b6893b84d402f3fb7e45
angelabenavente <angela.b.almazan@gmail.com>
2020-04-02 14:42:00 +0200

    Finish basic structure without functionality yet

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -8,7 +8,7 @@ import { Recipe } from '../recipe.model';
 })
 export class RecipeListComponent implements OnInit {
   recipes: Recipe[] =  [
-    new Recipe('A Test Recipe', 'A Test Recipe Simple', 'https://pxhere.com/en/photo/1575227?__cf_chl_jschl_tk__=646e867de3043feea120358d2e417b8a68ee47f5-1585741746-0-AXz2xFbrhGw7YAr-EIQTksDF2PSxFCBoeYgIZVqZ0arTRQEsmiqNMJ42E4GHOFrCs8V_yomxJa5o1eKqVdixYHzCouriPM2fgnZRD7NHdWT_5edPGXqBjBVGVi2SBX1Z12tYZvhrQSaORcRPl0FPATaHDMHvx1lAbgLNahFeaBN65-5784mD0RuT1kvM1btuutqiGpUentlYZd-Hu2mtiNE0c_ncRVHEJ7S6D8pEBHOrRgcm5dspJG_ztzK3GuOD_FbgaG7nmskSzLtGDrhrYPBIMfWqgi-OOMzH6D2QQvu5')
+    new Recipe('A Test Recipe', 'A Test Recipe Simple', 'https://cdn.pixabay.com/photo/2014/12/21/23/28/recipe-575434_960_720.png')
   ];
   constructor() { }
 
